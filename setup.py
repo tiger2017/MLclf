f0e6da23a48c86104d0df9ac20f4e5eca957e2fc
Tiger <supercxman@gmail.com>
2022-06-30 22:07:55 -0600

    modified the setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     name='MLclf',
     author="Daniel Cao",
     author_email="supercxman@gmail.com",
-    description='mini-imagenet dataset transformed to fit classification task.',
+    description='mini-imagenet dataset transformed to fit classification task or keep the format for meta-learning tasks.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     url="https://github.com/tiger2017/mlclf",
-    version='0.2.8',
+    version='0.2.9',
     license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3.9",
@@ -20,6 +20,6 @@ setup(
         'Topic :: Scientific/Engineering',
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     install_requires=['numpy', 'torch', 'torchvision']
 )
